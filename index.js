6c8164f8354cf9b5e67e0edb0bf05824b7233d3d
unknown <charles.attendu@gmail.com>
2018-11-26 14:37:58 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,236 @@
+'use strict';
+
+var http = require("http");
+var fs = require('fs');
+var urlParse = require('url').parse;
+var pathParse = require('path').parse;
+var querystring = require('querystring');
+
+var port = 1337;
+var hostUrl = 'http://localhost:'+port+'/';
+var defaultPage = '/index.html';
+
+var mimes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+};
+
+// --- Helpers ---
+var readFile = function (path) {
+    return fs.readFileSync(path).toString('utf8');
+};
+
+var writeFile = function (path, texte) {
+    fs.writeFileSync(path, texte);
+};
+
+// --- Server handler ---
+var redirect = function (reponse, path, query) {
+    var newLocation = path + (query == null ? '' : '?' + query);
+    reponse.writeHeader(302, {'Location' : newLocation });
+    reponse.end('302 page déplacé');
+};
+
+var getDocument = function (url) {
+    var pathname = url.pathname;
+    var parsedPath = pathParse(url.pathname);
+    var result = { data: null, status: 200, type: null };
+
+    if(parsedPath.ext in mimes) {
+        result.type = mimes[parsedPath.ext];
+    } else {
+        result.type = 'text/plain';
+    }
+
+    try {
+        result.data = readFile('./public' + pathname);
+        console.log('['+new Date().toLocaleString('iso') + "] GET " + url.path);
+    } catch (e) {
+        // File not found.
+        console.log('['+new Date().toLocaleString('iso') + "] GET " +
+                    url.path + ' not found');
+        result.data = readFile('template/error404.html');
+        result.type = 'text/html';
+        result.status = 404;
+    }
+
+    return result;
+};
+var sendPage = function (reponse, page) {
+    reponse.writeHeader(page.status, {'Content-Type' : page.type});
+    reponse.end(page.data);
+};
+
+var indexQuery = function (query) {
+
+    var resultat = { exists: false, id: null };
+
+    if (query !== null) {
+
+        query = querystring.parse(query);
+        if ('id' in query && 'titre' in query &&
+            query.id.length > 0 && query.titre.length > 0) {
+
+            resultat.exists = creerSondage(
+                query.titre, query.id,
+                query.dateDebut, query.dateFin,
+                query.heureDebut, query.heureFin);
+        }
+
+        if (resultat.exists) {
+            resultat.id = query.id;
+        }
+    }
+
+    return resultat;
+};
+
+var calQuery = function (id, query) {
+    if (query !== null) {
+        query = querystring.parse(query);
+        // query = { nom: ..., disponibilites: ... }
+        ajouterParticipant(id, query.nom, query.disponibilites);
+        return true;
+    }
+    return false;
+};
+
+var getIndex = function (replacements) {
+    return {
+        status: 200,
+        data: readFile('template/index.html'),
+        type: 'text/html'
+    };
+};
+
+
+// --- À compléter ---
+
+var mois = [
+    'Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin',
+    'Juil', 'Août', 'Sep', 'Oct', 'Nov', 'Dec'
+];
+
+var MILLIS_PAR_JOUR = (24 * 60 * 60 * 1000);
+
+// Retourne le texte HTML à afficher à l'utilisateur pour répondre au
+// sondage demandé.
+//
+// Doit retourner false si le calendrier demandé n'existe pas
+var getCalendar = function (sondageId) {
+    // TODO
+    return 'Calendrier <b>' + sondageId + '</b> (TODO)';
+};
+
+// Retourne le texte HTML à afficher à l'utilisateur pour voir les
+// résultats du sondage demandé
+//
+// Doit retourner false si le calendrier demandé n'existe pas
+var getResults = function (sondageId) {
+    // TODO
+    return 'Resultats du sondage <b>' + sondageId + '</b> (TODO)';
+};
+
+// Crée un sondage à partir des informations entrées
+//
+// Doit retourner false si les informations ne sont pas valides, ou
+// true si le sondage a été créé correctement.
+var creerSondage = function(titre, id, dateDebut, dateFin, heureDebut, heureFin) {
+    // TODO
+    return true;
+};
+
+// Ajoute un participant et ses disponibilités aux résultats d'un
+// sondage. Les disponibilités sont envoyées au format textuel
+// fourni par la fonction compacterDisponibilites() de public/calendar.js
+//
+// Cette fonction ne retourne rien
+var ajouterParticipant = function(sondageId, nom, disponibilites) {
+    // TODO
+};
+
+// Génère la `i`ème couleur parmi un nombre total `total` au format
+// hexadécimal HTML
+//
+// Notez que pour un grand nombre de couleurs (ex.: 250), générer
+// toutes les couleurs et les afficher devrait donner un joli dégradé qui
+// commence en rouge, qui passe par toutes les autres couleurs et qui
+// revient à rouge.
+var genColor = function(i, nbTotal) {
+    // TODO
+    return '#000000';
+};
+
+
+/*
+ * Création du serveur HTTP
+ * Note : pas besoin de toucher au code ici (sauf peut-être si vous
+ * faites les bonus)
+ */
+http.createServer(function (requete, reponse) {
+    var url = urlParse(requete.url);
+
+    // Redirect to index.html
+    if (url.pathname == '/') {
+        redirect(reponse, defaultPage, url.query);
+        return;
+    }
+
+    var doc;
+
+    if (url.pathname == defaultPage) {
+        var res = indexQuery(url.query);
+
+        if (res.exists) {
+            redirect(reponse, res.id);
+            return;
+        } else {
+            doc = getIndex(res.data);
+        }
+    } else {
+        var parsedPath = pathParse(url.pathname);
+
+        if (parsedPath.ext.length == 0) {
+            var id;
+
+            if (parsedPath.dir == '/') {
+                id = parsedPath.base;
+
+                if (calQuery(id, url.query)) {
+                    redirect(reponse, '/'+ id + '/results')
+                    return ;
+                }
+
+                var data = getCalendar(id);
+
+                if(data === false) {
+                    redirect(reponse, '/error404.html');
+                    return;
+                }
+
+                doc = {status: 200, data: data, type: 'text/html'};
+            } else {
+                if (parsedPath.base == 'results') {
+                    id = parsedPath.dir.slice(1);
+                    var data = getResults(id);
+
+                    if(data === false) {
+                        redirect(reponse, '/error404.html');
+                        return;
+                    }
+
+                    doc = {status: 200, data: data, type: 'text/html'};
+                } else {
+                    redirect(reponse, '/error404.html');
+                    return;
+                }
+            }
+        } else {
+            doc = getDocument(url);
+        }
+    }
+
+    sendPage(reponse, doc);
+
+}).listen(port);
---
+++ public/calendar.js
@@ -0,0 +1,31 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+    // TODO: Ajoutez ici du code qui doit s'exécuter au chargement de
+    // la page
+});
+
+function onClick(event) {
+    // TODO
+
+    /* La variable t contient l'élément HTML sur lequel le clic a été
+       fait. Notez qu'il ne s'agit pas forcément d'une case <td> du
+       tableau */
+    var t = event.target;
+
+    // Attribut id de l'élément sur lequel le clic a été fait
+    var id = t.id;
+}
+
+function onMove(event) {
+    // TODO
+
+    var t = event.target;
+    var id = t.id;
+}
+
+var compacterDisponibilites = function() {
+    // TODO
+
+    return '0000000';
+};
